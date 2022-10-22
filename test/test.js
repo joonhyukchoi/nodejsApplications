c2d84dea8341b1c4ee67c73da0e6dc91e7818c85
joonhyukchoi <cjh4787@gmail.com>
2022-10-22 16:18:11 +0900

    async await test

--- test/test.js
+++ test/test.js
@@ -1,15 +1,62 @@
-const promiseTest = () => {
-    return new Promise((resolve, reject) => {
-        console.log("promise callback")
-        reject(new Error('err'))
-    })
+// const promiseTest = () => {
+//     return new Promise((resolve, reject) => {
+//         console.log("promise callback")
+//         resolve('resolve')
+//         // reject(new Error('err'))
+//     })
+// }
+
+// promiseTest()
+// .then((res) => {
+//     console.log('first then')
+//     console.log(res)})
+// .then((resolve) => {
+//     console.log("second then method")
+//     })
+// .catch(console.log)
+
+// function getUser(username) {
+//     console.log(username)
+//     return username
+// }
+// async function getUserAll() {
+//     let user;
+//     console.log('function getUserAll')
+//     user = await getUser('jeresig');
+//     // console.log(user);
+  
+//     // user = await getUser('ahejlsberg');
+//     // // console.log(user);
+  
+//     // user = await getUser('ungmo2');
+//     console.log('user');
+//   }
+
+// getUserAll()
+
+// console.log("in first stack")
+import fetch from "node-fetch";
+
+// Promise를 반환하는 함수 정의
+function getUser(username) {
+  return fetch(`https://api.github.com/users/${username}`)
+    .then(res => res.json())
+    .then(user => {
+        console.log('in then')
+        return user.name});
 }
 
-promiseTest()
-.then(console.log)
-.then((resolve) => {
-    console.log("second then method")
-    })
-.catch(console.log)
+async function getUserAll() {
+  let user;
+  user = await getUser('jeresig');
+//   console.log(user);
+console.log('user1');
+console.log(user);
+  user = await getUser('ahejlsberg');
+  console.log(user);
 
-console.log("in first stack")
+  user = await getUser('ungmo2');
+  console.log(user);
+}
+
+getUserAll();
