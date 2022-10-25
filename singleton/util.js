9515c2976812dff98d3300819b5d2506fda9719c
joonhyukchoi <cjh4787@gmail.com>
2022-10-25 19:57:39 +0900

    feat: singleton 패턴적용 예시: db 커넥션

---
+++ singleton/util.js
@@ -0,0 +1,22 @@
+export function promisify(callbackBasedApi) {
+  return function promisified(...args) {
+    // 프라미스 생성자를 사용하여 새로운 프라미스를 생성하고 즉시 호출자에 그것을 반환함
+    return new Promise((resolve, reject) => { // (1)
+      const newArgs = [
+        ...args,
+        // 프라미스 생성자에 전달되는 함수에서 callbackBasedApidㅔ 특별한 콜백을 전달.
+        // 우리는 콜백이 항상 마지막에 온다는 것을 알고 있으므로 promisified() 함수에 전달되는 인자목록(args)에
+        // 콜백을 추가해주기만 하면 됨. 추가한 특별한 콜백에서 에러를 받는다면 즉시 프라미스를 거부하고 그렇지 않다면 주이진 result를 갖고 이행
+        function (err, result) {  // (2)
+          if (err) {
+            return reject(err)
+          }
+
+          resolve(result)
+        }
+      ]
+      // 마지막으로 인자목록을 갖고 callbackBasedApi 호출
+      callbackBasedApi(...newArgs)  // (3)
+    })
+  }
+}
