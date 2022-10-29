a6b5d6b747a17c87ef7c144cbb56c64ab82adb72
joonhyukchoi <cjh4787@gmail.com>
2022-10-29 21:17:51 +0900

    refactor: 의존성 주입 구현

--- singleton/db.js
+++ singleton/db.js
@@ -1,7 +1,13 @@
-import { dirname, join } from 'path'
-import { fileURLToPath } from 'url'
+// import { dirname, join } from 'path'
+// import { fileURLToPath } from 'url'
+// import sqlite3 from 'sqlite3'
+// const __dirname = dirname(fileURLToPath(import.meta.url))
+// export const db = new sqlite3.Database(
+//     join(__dirname, 'data.sqlite')
+// )
+
 import sqlite3 from 'sqlite3'
-const __dirname = dirname(fileURLToPath(import.meta.url))
-export const db = new sqlite3.Database(
-    join(__dirname, 'data.sqlite')
-)
+
+export function createDb (dbFile) {
+    return new sqlite3.Database(dbFile)
+}
