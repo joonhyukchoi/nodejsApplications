41d084d7b52cac6373bd3014208c3b9f77305b6f
joonhyukchoi <cjh4787@gmail.com>
2022-10-29 21:21:05 +0900

    refactor: 의존성 주입 구현, 더이상 데이터베이스 모듈을 임포트 하지 낳음, Blog 클래스 생성자는 db를 인자로 취함

--- singleton/blog.js
+++ singleton/blog.js
@@ -1,10 +1,12 @@
 import { promisify } from 'util'
-import { db } from './db.js'
-
-const dbRun = promisify(db.run.bind(db))
-const dbAll = promisify(db.all.bind(db))
 
 export class Blog {
+    constructor (db) {
+        this.db = db
+        this.dbRun = promisify(db.run.bind(db))
+        this.dbAll = promisify(db.all.bind(db))
+    }
+
     initialize () {
         const initQuery = `CREATE TABLE IF NOT EXISTS posts (
             id TEXT PRIMARY KEY,
@@ -12,15 +14,42 @@ export class Blog {
             content TEXT,
             created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
         );`
-        return dbRun(initQuery)
+        return this.dbRun(initQuery)
     }
 
     createPost (id, title, content, createdAt) {
-        return dbRun('INSERT INTO posts VALUES (?, ?, ?, ?)',
+        return this.dbRun('INSERT INTO posts VALUES (?, ?, ?, ?)',
     id, title, content, createdAt)
     }
 
     getAllPosts() {
-        return dbAll('SELECT * FROM posts ORDER BY created_at DESC')
+        return this.dbAll('SELECT * FROM posts ORDER BY created_at DESC')
     }
 }
+
+// import { promisify } from 'util'
+// import { db } from './db.js'
+
+// const dbRun = promisify(db.run.bind(db))
+// const dbAll = promisify(db.all.bind(db))
+
+// export class Blog {
+//     initialize () {
+//         const initQuery = `CREATE TABLE IF NOT EXISTS posts (
+//             id TEXT PRIMARY KEY,
+//             title TEXT NOT NULL,
+//             content TEXT,
+//             created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+//         );`
+//         return dbRun(initQuery)
+//     }
+
+//     createPost (id, title, content, createdAt) {
+//         return dbRun('INSERT INTO posts VALUES (?, ?, ?, ?)',
+//     id, title, content, createdAt)
+//     }
+
+//     getAllPosts() {
+//         return dbAll('SELECT * FROM posts ORDER BY created_at DESC')
+//     }
+// }
