b19a14e06b96c31da3d1276e08993d195d5b16a8
joonhyukchoi <cjh4787@gmail.com>
2022-10-29 21:30:38 +0900

    refactor: 의존성 주입 구현

--- singleton/index.js
+++ singleton/index.js
@@ -1,7 +1,15 @@
 import { Blog } from './blog.js'
+import { dirname, join } from 'path'
+import { fileURLToPath } from 'url'
+import { createDb } from './db.js'
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
 
 async function main () {
-    const blog = new Blog()
+    // 팩토리 함수 createDb()를 사용하여 데이터베이스 종속서(db)을 생성
+    const db = createDb(join(__dirname, 'data.sqlite'))
+    // Blog 클래스를 인스턴스화 할 때 데이터베이스 인스턴스를 명시적으로 '주입'
+    const blog = new Blog(db)
     await blog.initialize()
     const posts = await blog.getAllPosts()
     if (posts.length === 0) {
