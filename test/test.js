1d14bac43db9dfdc4f015724e94a7bc6c43839b9
joonhyukchoi <cjh4787@gmail.com>
2022-11-05 22:25:15 +0900

    underscore 라이브러리 사용 함수형 자바스크립트 예제

--- test/test.js
+++ test/test.js
@@ -1,65 +1,23 @@
-// const promiseTest = () => {
-//     return new Promise((resolve, reject) => {
-//         console.log("promise callback")
-//         resolve('resolve')
-//         // reject(new Error('err'))
-//     })
-// }
+const _ = require('underscore')
 
-// promiseTest()
-// .then((res) => {
-//     console.log('first then')
-//     console.log(res)})
-// .then((resolve) => {
-//     console.log("second then method")
-//     })
-// .catch(console.log)
+[1, 2, 3].forEach(alert)
 
-// function getUser(username) {
-//     console.log(username)
-//     return username
-// }
-// async function getUserAll() {
-//     let user;
-//     console.log('function getUserAll')
-//     user = await getUser('jeresig');
-//     // console.log(user);
-  
-//     // user = await getUser('ahejlsberg');
-//     // // console.log(user);
-  
-//     // user = await getUser('ungmo2');
-//     console.log('user');
-//   }
-
-// getUserAll()
-
-// console.log("in first stack")
-import fetch from "node-fetch";
-
-// Promise를 반환하는 함수 정의
-function getUser(username) {
-  return fetch(`https://api.github.com/users/${username}`)
-    .then(res => res.json())
-    .then(user => {
-        console.log('in then')
-        return user.name});
-}
-function test1() {
-    setTimeout(() => console.log('hello'), 0)
+function splat(fun) {
+  return function(array) {
+    return fun.apply(null, array)
+  }
 }
 
-async function getUserAll() {
-  let user;
-  user = await getUser('jeresig');
-//   console.log(user);
-console.log('user1');
-console.log(user);
-  await test1();
-  console.log('test1');
+var addArrayelements = splat(function(x, y) {return x + y})
 
-  user = await getUser('ungmo2');
-  console.log(user);
+addArrayelements([1, 2])
+
+function unsplat(fun) {
+  return function() {
+    return fun.call(null, _.toArray(arguments))
+  }
 }
 
-getUserAll();
+var joinElements = unsplat(function(array) {return array.join(' ')})
+
+joinElements(1, 2, 3)
