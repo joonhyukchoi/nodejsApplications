353fa047ad722f6d7191ffe08afc1722a5a44bc9
joonhyukchoi <cjh4787@gmail.com>
2022-11-30 22:56:46 +0900

    refactor: 제너레이터로 변경

--- iterator/matrix.js
+++ iterator/matrix.js
@@ -20,24 +20,42 @@ export class Matrix {
             this.data[row][column] = value
     }
     // 반복자 반환
-    [Symbol.iterator] () {
+    // [Symbol.iterator] () {
+    //     let nextRow = 0
+    //     let nextCol = 0
+
+    //     return {
+    //         next: () => {
+    //             if (nextRow === this.data.length) {
+    //                 return { done: true }
+    //             }
+
+    //             const currVal = this.data[nextRow][nextCol]
+    //             if (nextCol === this.data[nextRow].length - 1) {
+    //                 nextRow ++
+    //                 nextCol = 0
+    //             } else {
+    //                 nextCol ++
+    //             }
+    //             return { value: currVal}
+    //         }
+    //     }
+    // }
+
+    // 제너레이터로 구현. @@iterator 함수에 * 을 붙여서 제너레이터로 사용
+    * [Symbol.iterator] () {
+        // 이전처럼 클로저가 아니라 그냥 로컬 변수. 제너레이터가 호출될 때 재진입 사이에 로컬의 상태가 유지되기 때문에 이렇게 가능
         let nextRow = 0
         let nextCol = 0
 
-        return {
-            next: () => {
-                if (nextRow === this.data.length) {
-                    return { done: true }
-                }
+        while (nextRow !== this.data.length) {
+            yield this.data[nextRow][nextCol]
 
-                const currVal = this.data[nextRow][nextCol]
-                if (nextCol === this.data[nextRow].length - 1) {
-                    nextRow ++
-                    nextCol = 0
-                } else {
-                    nextCol ++
-                }
-                return { value: currVal}
+            if (nextCol === this.data[nextRow].length - 1) {
+                nextRow ++
+                nextCol = 0
+            } else {
+                nextCol ++
             }
         }
     }
