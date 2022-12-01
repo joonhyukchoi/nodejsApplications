0f912d0c7d3d33adc6e13dc3a7ae371fc8748439
joonhyukchoi <cjh4787@gmail.com>
2022-12-01 23:14:52 +0900

    refactor: socket.io

--- zoom-clonecoding/websockets-starter-forked/src/public/app.js
+++ zoom-clonecoding/websockets-starter-forked/src/public/app.js
@@ -1,41 +1,57 @@
-const messageList = document.querySelector("ul")
-const nickForm = document.querySelector("#nick")
-const messageForm = document.querySelector("#message")
-const socket = new WebSocket(`ws://${window.location.host}`)
+const socket = io();
 
-socket.addEventListener("open", () => {
-    console.log("Connected to server")
-})
+const welcome = document.getElementById("welcome");
+const form = welcome.querySelector("form");
 
-socket.addEventListener("message", (message) => {
-    const li = document.createElement("li")
-    li.innerText = message.data
-    messageList.append(li)
-})
-
-socket.addEventListener("close", () => {
-    console.log("Disconnected to server")
-})
-
-function makeMessage (type, payload) {
-    const message = {type, payload}
-    return JSON.stringify(message)
+function handleRoomSubmit(event) {
+  event.preventDefault();
+  const input = form.querySelector("input");
+  socket.emit("enter_room", { payload: input.value }, () => {
+    console.log("server is done!");
+  });
+  input.value = "";
 }
-function handleSubmit(event) {
-    event.preventDefault()
-    const input = messageForm.querySelector("input")
-    socket.send(makeMessage("new_message", input.value))
-    const li = document.createElement("li")
-    li.innerText = `You: ${input.value}`
-    messageList.append(li)
-    input.value = ""
-}
+form.addEventListener("submit", handleRoomSubmit);
 
-function handleNickSubmit(event) {
-    event.preventDefault()
-    const input = nickForm.querySelector("input")
-    socket.send(makeMessage("nickname", input.value))
-    input.value = ""
-}
-messageForm.addEventListener("submit", handleSubmit);    
-nickForm.addEventListener("submit", handleNickSubmit)
+
+// const messageList = document.querySelector("ul")
+// const nickForm = document.querySelector("#nick")
+// const messageForm = document.querySelector("#message")
+// const socket = new WebSocket(`ws://${window.location.host}`)
+
+// socket.addEventListener("open", () => {
+//     console.log("Connected to server")
+// })
+
+// socket.addEventListener("message", (message) => {
+//     const li = document.createElement("li")
+//     li.innerText = message.data
+//     messageList.append(li)
+// })
+
+// socket.addEventListener("close", () => {
+//     console.log("Disconnected to server")
+// })
+
+// function makeMessage (type, payload) {
+//     const message = {type, payload}
+//     return JSON.stringify(message)
+// }
+// function handleSubmit(event) {
+//     event.preventDefault()
+//     const input = messageForm.querySelector("input")
+//     socket.send(makeMessage("new_message", input.value))
+//     const li = document.createElement("li")
+//     li.innerText = `You: ${input.value}`
+//     messageList.append(li)
+//     input.value = ""
+// }
+
+// function handleNickSubmit(event) {
+//     event.preventDefault()
+//     const input = nickForm.querySelector("input")
+//     socket.send(makeMessage("nickname", input.value))
+//     input.value = ""
+// }
+// messageForm.addEventListener("submit", handleSubmit);    
+// nickForm.addEventListener("submit", handleNickSubmit)
--- zoom-clonecoding/websockets-starter-forked/src/server.js
+++ zoom-clonecoding/websockets-starter-forked/src/server.js
@@ -1,8 +1,7 @@
 import http from "http";
-import WebSocket from "ws";
+import SocketIO from "socket.io";
 import express from "express";
-import path from 'path';
-const __dirname = path.resolve();
+
 const app = express();
 
 app.set("view engine", "pug");
@@ -11,30 +10,58 @@ app.use("/public", express.static(__dirname + "/public"));
 app.get("/", (_, res) => res.render("home"));
 app.get("/*", (_, res) => res.redirect("/"));
 
+const httpServer = http.createServer(app);
+const wsServer = SocketIO(httpServer);
+
+wsServer.on("connection", (socket) => {
+  socket.on("enter_room", (msg, done) => {
+    console.log(msg);
+    setTimeout(() => {
+      done();
+    }, 2000);
+  });
+});
+
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
+httpServer.listen(process.env.PORT, handleListen);
 
-const server = http.createServer(app);
-const wss = new WebSocket.Server({ server });
+// import http from "http";
+// import WebSocket from "ws";
+// import express from "express";
+// import path from 'path';
+// const __dirname = path.resolve();
+// const app = express();
 
-const sockets = []
+// app.set("view engine", "pug");
+// app.set("views", __dirname + "/views");
+// app.use("/public", express.static(__dirname + "/public"));
+// app.get("/", (_, res) => res.render("home"));
+// app.get("/*", (_, res) => res.redirect("/"));
 
-wss.addListener("connection", (socket) => {
-    sockets.push(socket)
-    socket["nickname"] = "Anon"
-    console.log("Connected to browser")
-    socket.on("close", () => {
-        console.log("Disconnect from browser")
-    })
-    socket.on("message", (message) => {
-        const parsed = JSON.parse(message)
-        if (parsed.type === "new_message") {
-            sockets.forEach(aSocket => {
-                if (socket["nickname"] !== aSocket["nickname"]) {
-                    aSocket.send(`${socket.nickname}: ${parsed.payload}`)
-                }})
-        } else if (parsed.type === "nickname") {
-            socket["nickname"] = parsed.payload
-        }
-    })
-})
-server.listen(3000, handleListen);
+// const handleListen = () => console.log(`Listening on http://localhost:3000`);
+
+// const server = http.createServer(app);
+// const wss = new WebSocket.Server({ server });
+
+// const sockets = []
+
+// wss.addListener("connection", (socket) => {
+//     sockets.push(socket)
+//     socket["nickname"] = "Anon"
+//     console.log("Connected to browser")
+//     socket.on("close", () => {
+//         console.log("Disconnect from browser")
+//     })
+//     socket.on("message", (message) => {
+//         const parsed = JSON.parse(message)
+//         if (parsed.type === "new_message") {
+//             sockets.forEach(aSocket => {
+//                 if (socket["nickname"] !== aSocket["nickname"]) {
+//                     aSocket.send(`${socket.nickname}: ${parsed.payload}`)
+//                 }})
+//         } else if (parsed.type === "nickname") {
+//             socket["nickname"] = parsed.payload
+//         }
+//     })
+// })
+// server.listen(3000, handleListen);
