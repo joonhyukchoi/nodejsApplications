79339607df9cdaded5f14bf2797c78bd7336748d
joonhyukchoi <cjh4787@gmail.com>
2022-12-08 22:13:37 +0900

    refactor: 복습필요, 비동기 방식으로 리팩토링

--- general-purpose-javascript/server.js
+++ general-purpose-javascript/server.js
@@ -5,14 +5,15 @@ import reactServer from 'react-dom/server.js'
 import htm from 'htm'
 import fastify from 'fastify'
 import fastifyStatic from 'fastify-static'
-import { StaticRouter } from 'react-router-dom'
+import { StaticRouter, matchPath } from 'react-router-dom'
+import { routes } from './frontend/routes.js'
 import { App } from './frontend/App.js'
 
 const __dirname = dirname(fileURLToPath(import.meta.url))
 const html = htm.bind(react.createElement)
 
 // 
-const template = ({ content }) => `<!DOCTYPE html>
+const template = ({ content, serverData }) => `<!DOCTYPE html>
 <html>
   <head>
     <meta charset="UTF-8">
@@ -20,6 +21,9 @@ const template = ({ content }) => `<!DOCTYPE html>
   </head>
   <body>
     <div id="root">${content}</div>
+    ${serverData ? `<script type = "text/javascript>
+    window.__STATIC_CONTEXT__=${JSON.stringify(serverData)}
+    </script>` : ''}
     <script type="text/javascript" src="/public/main.js"></script>
   </body>
 </html>`
@@ -38,7 +42,35 @@ server.register(fastifyStatic, {
 // React 애플리케이션이 렌더링할 때 현재 URL을 기반으로 적절한 페이지 컴포넌트를 표시함
 server.get('*', async (req, reply) => {
   const location = req.raw.originalUrl
-  const staticContext = {}
+  let component
+  let match
+  for (const route of routes) {
+    component = route.component
+    // match에는 매칭된 정보 포함. ex) 경로에 포함된 params 정보
+    match = matchPath(location, route)
+    if (match) {
+      break
+    }
+  }
+
+  let staticData
+  let staticError
+  let hasStaticContext = false
+  if (typeof component.preloadAsyncData === 'function') {
+    hasStaticContext = true
+    try {
+      const data = await component.preloadAsyncData({ match })
+      staticData = data
+    } catch (err) {
+      staticError = err
+    }
+  }
+  const staticContext = {
+    [location]: {
+      data: staticData,
+      err: staticError
+    }
+  }
   // 서버측에서 react-router-dom의 staticRouter 인스턴스를 사용하여 애플리케이션의 컴포넌트를 감싸야 함
   // StaticRouter는 서버 측 렌더링에 사용할 수 있는 React Router 버전임
   // 이 라우터는 브라우저 윈도우에서 현재의 URL을 획득하지 않고 location 속성을 통해 서버로부터 직접 현재 URL을 전달할 수 있음
@@ -54,13 +86,10 @@ server.get('*', async (req, reply) => {
   // 생성된 HTML은 주어진 URL에서 클라이언트 측 애플리케이션에 의해 생성한 HTML과 동일함
   // 다음 몇 줄에서는 template() 함수를 사용하여 페이지 템플릿으로 생성된 HTML코드인 content를 감싸고 그 결과를 클라이언트에 전송
   const content = reactServer.renderToString(serverApp)
-  const responseHtml = template({ content })
+  const serverData = hasStaticContext ? staticContext : null
+  const responseHtml = template({ content, serverData })
 
-  let code = 200
-  if (staticContext.statusCode) {
-    code = staticContext.statusCode
-  }
-
+  const code = staticContext.statusCode ? staticContext.statusCode : 200
   reply.code(code).type('text/html').send(responseHtml)
 })
 
