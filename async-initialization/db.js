5f740e420a4a4743e872a9d1802d1667eb0f1027
joonhyukchoi <cjh4787@gmail.com>
2022-12-10 21:59:14 +0900

    refactor: DB 클래스 재구현

--- async-initialization/db.js
+++ async-initialization/db.js
@@ -1,31 +1,49 @@
 import { EventEmitter } from 'events'
 
 class DB extends EventEmitter {
-    connected = false
-    commandsQueue = []
+    // connected = false
+    // commandsQueue = []
+    constructor () {
+        super()
+        this.state = new QueuingState(this)
+    }
+
+    // async query (queryString) {
+    //     if (!this.connected) {
+    //         console.log(`Request queued: ${queryString}`)
+
+    //         return new Promise((resolve, reject) => {
+    //             const command = () => {
+    //                 this.query(queryString)
+    //                     .then(resolve, reject)
+    //             }
+    //             this.commandsQueue.push(command)
+    //         })
+    //     }
+    //     console.log(`Query executed: ${queryString}`)
+    // }
 
     async query (queryString) {
-        if (!this.connected) {
-            console.log(`Request queued: ${queryString}`)
-
-            return new Promise((resolve, reject) => {
-                const command = () => {
-                    this.query(queryString)
-                        .then(resolve, reject)
-                }
-                this.commandsQueue.push(command)
-            })
-        }
-        console.log(`Query executed: ${queryString}`)
+        return this.state.query(queryString)
     }
+
+    // connect () {
+    //     setTimeout(() => {
+    //         this.connected = true
+    //         this.emit('connected')
+    //         this.commandsQueue.forEach(command => command())
+    //         this.commandsQueue = []
+    //     }, 500)
+    // }
 
     connect () {
         setTimeout(() => {
             this.connected = true
             this.emit('connected')
-            this.commandsQueue.forEach(command => command())
-            this.commandsQueue = []
-        }, 500)
+            const oldState = this.state
+            this.state = new InitializedState(this)
+            oldState[deactivate] && oldState[deactivate]()
+        })
     }
 }
 
