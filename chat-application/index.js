d8cca2d1423d77b9c026ebad620884ddaa54cdbc
joonhyukchoi <cjh4787@gmail.com>
2022-12-16 19:37:45 +0900

    refactor: broadcasting -> redis 사용 메시지 브로커

--- chat-application/index.js
+++ chat-application/index.js
@@ -1,6 +1,11 @@
 import { createServer } from 'http'
 import staticHandler from 'serve-handler'
 import WebSocket, { WebSocketServer } from 'ws'
+import Redis from 'ioredis'
+import { channel } from 'diagnostics_channel'
+
+const redisSub = new Redis()
+const redisPub = new Redis()
 
 const server = createServer((req, res) => {
     // 모든 요청을 특수핸들러로 전달하고 www 디렉터리에서 모든 정적 파일 처리
@@ -12,16 +17,25 @@ wss.on('connection', client => {
     console.log('Client connected')
     client.on('message', msg => {
         console.log(`Message: ${msg}`)
-        broadcast(msg)
+        // broadcast(msg)
+        redisPub.publish('chat_messages', msg)
     })
 })
 
-function broadcast (msg) {
+redisSub.subscribe('chat_messages')
+redisSub.on('message', (channel, msg) => {
     for (const client of wss.clients) {
-        if (client.readyState === ws.OPEN) {
+        if (client.readyState === WebSocket.OPEN) {
             client.send(msg)
         }
     }
-}
+})
+// function broadcast (msg) {
+//     for (const client of wss.clients) {
+//         if (client.readyState === ws.OPEN) {
+//             client.send(msg)
+//         }
+//     }
+// }
 
 server.listen(process.argv[2] || 8080)
