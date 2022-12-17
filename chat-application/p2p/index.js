dd77a66f5582380d3e8b79164052645c112452b9
joonhyukchoi <cjh4787@gmail.com>
2022-12-17 19:00:26 +0900

    feat: zeromq 이용 p2p 아키텍처 구현

---
+++ chat-application/p2p/index.js
@@ -0,0 +1,49 @@
+import { createServer } from 'http'
+import staticHandler from 'serve-handler'
+import WebSocket, { WebSocketServer } from 'ws'
+import yargs from 'yargs'
+import { Publisher, Subscriber } from 'zeromq'
+
+const server = createServer((req, res) => {
+    return staticHandler(req, res, { public: 'www'})
+})
+
+let pubSocket
+async function initializeSokcets() {
+    pubSocket = new Publisher()
+    await pubSocket.bind(`tcp://127.0.0.1:${yargs.argv.pub}`)
+    const subSocket = new Subscriber()
+    const subPorts = [].concat(yargs.argv.sub)
+    for (const port of subPorts) {
+        console.log(`Subscribing to ${port}`)
+        subSocket.connect(`tcp://127.0.0.1:${port}`)
+    }
+    subSocket.subscribe('chat')
+
+    for await (const [msg] of subSocket) {
+        console.log(`Message from another server:${msg}`)
+        broadcast(msg.toString().split(' ')[1])
+    }
+}
+
+initializeSokcets()
+
+const wss = new WebSocketServer({ server })
+wss.on('connection', client => {
+    console.log('Client connected')
+    client.on('message', msg => {
+        console.log(`Message: ${msg}`)
+        broadcast(msg)
+        pubSocket.send(`chat ${msg}`)
+    })
+})
+
+function broadcast (msg) {
+    for (const client of wss.clients) {
+        if (client.readyState === ws.OPEN) {
+            client.send(msg)
+        }
+    }
+}
+
+server.listen(process.argv[2] || 8080)
