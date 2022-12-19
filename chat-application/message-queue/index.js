d89d61484b8f3915edcee4392137f194b2965207
joonhyukchoi <cjh4787@gmail.com>
2022-12-19 11:08:03 +0900

    feat: AMQP 이용 메시지큐 히스토리 기능 구현

---
+++ chat-application/message-queue/index.js
@@ -0,0 +1,32 @@
+import { createServer } from 'http'
+import level from 'level'
+import timestamp from 'monotonic-timestamp'
+import JSONStream from 'JSONStream'
+import amqp from 'amqplib'
+import { create } from 'domain'
+
+async function main () {
+    const db = level('./msgHistory')
+
+    const connectiton = await amqp.connect('amqp://localhost')
+    const channel = await connectiton.createChannel()
+    await channel.assertExchange('chat', 'fanout')
+    const { queue } = channel.assertQueue('chat_history')
+    await channel.bindQueue(queue, 'chat')
+
+    channel.consume(queue, async msg => {
+        const content = msg.content.toString()
+        console.log(`Saving message: ${content}`)
+        await db.put(timestamp(), content)
+        channel.ack(msg)
+    })
+
+    createServer((req, res) => {
+        res.writeHead(200)
+        db.createValueStream()
+            .pipe(JSONStream.stringify())
+            .pipe(res)
+    }).listen(8090)
+}
+
+main().catch(err => console.log(err))
